8f4eef5c62c93b40a0fc4855d8b9dcc8bf4a2d40
localuser <globaluser@gmail.com>
2024-06-27 12:23:14 +0530

    BAL is modified

--- BAL.java
+++ BAL.java
@@ -1,3 +1,4 @@
+//This is business acess layer
 class project{
     Public static void main(String[] args){
         System.out.println("Hello world");
---
+++ PL.java
@@ -0,0 +1,6 @@
+//this is Business Access Layer
+class project{
+    Public static void main(String [] args){
+        System.out.println("Hello world");
+    }
+}
